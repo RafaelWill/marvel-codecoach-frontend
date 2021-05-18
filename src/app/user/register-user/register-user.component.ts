c2a35b89a56ffb95dfbb61029de51d11a6af8580
vincentvliegen <vincentvliegen@hotmail.com>
2021-05-18 09:54:44 +0200

    story 1 minimal team agreement | doing : pattern for confirmedPassword

--- src/app/user/register-user/register-user.component.ts
+++ src/app/user/register-user/register-user.component.ts
@@ -14,10 +14,13 @@ export class RegisterUserComponent implements OnInit {
     firstName: '',
     lastName: '',
     userCredential: this._formBuilder.group({
-      email: ['', {validators: [Validators.required, Validators.email], updateOn: 'blur'}],
-      password: ['', [Validators.required, Validators.minLength(8), Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$')]],
-      confirmedPassword: ['', [Validators.required]]
-    },
+        email: ['', {validators: [Validators.required, Validators.email], updateOn: 'blur'}],
+        password: ['',
+          [Validators.required,
+          Validators.minLength(8),
+          Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$')]],
+        confirmedPassword: ['', [Validators.required]]
+      },
       {validator: this.checkIfMatchingPasswords('password', 'confirmedPassword')})
   });
 
@@ -43,18 +46,21 @@ export class RegisterUserComponent implements OnInit {
     this.addPerson();
   }
 
-// tslint:disable-next-line:typedef
-checkIfMatchingPasswords(passwordKey: string, passwordConfirmationKey: string) {
-  return (group: FormGroup) => {
-    // tslint:disable-next-line:one-variable-per-declaration
-    const passwordInput = group.controls[passwordKey],
-      passwordConfirmationInput = group.controls[passwordConfirmationKey];
-    if (passwordInput.value !== passwordConfirmationInput.value) {
-      return passwordConfirmationInput.setErrors({notEquivalent: true});
-    }
-    else {
-      return passwordConfirmationInput.setErrors(null);
-    }
-  };
-}
+  checkIfMatchingPasswords(passwordKey: string, passwordConfirmationKey: string): (group: FormGroup) => void {
+    return (group) => {
+      const passwordInput = group.controls[passwordKey];
+      const passwordConfirmationInput = group.controls[passwordConfirmationKey];
+      if (passwordInput.value !== passwordConfirmationInput.value) {
+        passwordConfirmationInput.setErrors({notEquivalent: true});
+      } else {
+        passwordConfirmationInput.setErrors(null);
+      }
+    };
+  }
+
+  updateConfirmPattern(): void {
+    const password = this._registrationForm.get('userCredential')?.get('password')?.value;
+    const escapedPassword = String(password).replace(/[\\^$*+?.()|[\]{}]/g, '\\$&');
+    document.getElementById('confirmedPassword')?.setAttribute('pattern', escapedPassword);
+  }
 }
