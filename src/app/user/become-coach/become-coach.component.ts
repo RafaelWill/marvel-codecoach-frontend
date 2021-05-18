091caf016f082058a0c9a2e9e54ccc8b9a7c239c
RafaelWill <willekens.rafael@gmail.com>
2021-05-18 16:57:11 +0200

    Story 5 | doing : working on dynamic form

--- src/app/user/become-coach/become-coach.component.ts
+++ src/app/user/become-coach/become-coach.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormArray, FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-become-coach',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BecomeCoachComponent implements OnInit {
 
-  constructor() { }
+  _becomeCoachForm!: FormGroup;
+
+  constructor(private _formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this._becomeCoachForm = this._formBuilder.group({
+      fullName: '',
+      motivation: '',
+      topic: '',
+      extraTopics: this._formBuilder.array([])
+    });
   }
 
+  get becomeCoachForm(): FormGroup {
+    return this._becomeCoachForm;
+  }
+
+  // tslint:disable-next-line:typedef
+  get alternateTopics() {
+    return this._becomeCoachForm.get('extraTopics') as FormArray;
+  }
+
+  // tslint:disable-next-line:typedef
+  addAlternateTopic() {
+    this.alternateTopics.push(this._formBuilder.control(''));
+  }
+
+  // tslint:disable-next-line:typedef
+  submit() {
+    console.log(this.becomeCoachForm.value);
+  }
 }
