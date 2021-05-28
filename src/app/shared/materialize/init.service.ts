89b253c312cf79d0de64e4ec658ce95f5b62e0bc
LioKr <lio.krebs@gmail.com>
2021-05-28 11:21:21 +0200

    story 13 | doing : init script removed console.log

--- src/app/shared/materialize/init.service.ts
+++ src/app/shared/materialize/init.service.ts
@@ -10,14 +10,12 @@ export class InitService {
 
   initDropdowns(): void {
     setTimeout(() => {
-      console.log('init dropdowns');
       M.Dropdown.init(document.querySelectorAll('.dropdown-trigger'), {coverTrigger: false});
     }, 1);
   }
 
   initCollapsible(): void {
     setTimeout(() => {
-      console.log('init dropdowns');
       M.Collapsible.init(document.querySelectorAll('.collapsible'), {});
     }, 1);
   }
@@ -25,42 +23,36 @@ export class InitService {
   autoInit(): void {
     // try to avoid using this function. Since it might break the others
     setTimeout(() => {
-      console.log('auto init');
       M.AutoInit();
     }, 1);
   }
 
   initFormSelect(): void {
     setTimeout(() => {
-      console.log('init form select');
       M.FormSelect.init(document.querySelectorAll('select'), {classes: '', dropdownOptions: {coverTrigger: false}});
     }, 1);
   }
 
   initParalax(): void {
     setTimeout(() => {
-      console.log('init parallax');
       M.Parallax.init(document.querySelectorAll('.parallax'));
     }, 1);
   }
 
   initSidenav(): void {
     setTimeout(() => {
-      console.log('init sidenav');
       M.Sidenav.init(document.querySelectorAll('.sidenav'));
     }, 1);
   }
 
   initModal(): void {
     setTimeout(() => {
-      console.log('init modal');
       M.Modal.init(document.querySelectorAll('.modal'));
     }, 1);
   }
 
   initDatepicker(): void{
     setTimeout( () => {
-      console.log('init datepicker');
       const currDate = (new Date());
       const currYear = (new Date()).getFullYear();
       const options = {
@@ -82,7 +74,6 @@ export class InitService {
       autoClose: true
     };
     setTimeout( () => {
-    console.log('init timepicker');
     M.Timepicker.init(document.querySelectorAll('.timepicker'), options);
     }, 1);
   }
