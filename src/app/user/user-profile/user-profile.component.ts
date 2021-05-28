9d16325db41f4a957f592fd80cf1654a0938085f
RafaelWill <willekens.rafael@gmail.com>
2021-05-28 10:37:08 +0200

    Story 3 | doing : Restricted link to become a coach when already a coach Story 9 | doing : made sure you can navigate back to your own profile page Story 5 | doing : refactored select options with ngFor

--- src/app/user/user-profile/user-profile.component.ts
+++ src/app/user/user-profile/user-profile.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Person} from '../../shared/model/person';
 import {PersonService} from '../../shared/service/person.service';
+import {CookieService} from 'ngx-cookie-service';
+import {environment} from '../../../environments/environment';
 
 @Component({
   selector: 'app-user-profile',
@@ -11,24 +13,41 @@ import {PersonService} from '../../shared/service/person.service';
 export class UserProfileComponent implements OnInit {
   private _person!: Person;
   isLoading = true;
+  isCoach = false;
 
   constructor(
     private route: ActivatedRoute,
-    private personService: PersonService
+    private personService: PersonService,
+    private cookieService: CookieService
   ) { }
 
   ngOnInit(): void {
     const routeParams = this.route.snapshot.paramMap;
     const personIdFromRoute = String(routeParams.get('id'));
+    this.setCookie(personIdFromRoute);
     this.personService
       .findById(personIdFromRoute)
       .subscribe(person => { this._person = person;
-                             this.isLoading = false; },
+                             this.isLoading = false;
+                             this.assertIfCoach();
+                             },
                   error => { console.log(error);
                              this.isLoading = false; });
   }
 
   get person(): Person {
     return this._person;
+  }
+
+  private setCookie(userid: string): void {
+    const date = new Date();
+    date.setDate(date.getDate() + 3 );
+    this.cookieService.set('userid', userid, date, '/', environment.domain);
+  }
+
+  assertIfCoach(): void {
+    if (this.person.roles.includes('COACH')){
+      this.isCoach = true;
+    }
   }
 }
