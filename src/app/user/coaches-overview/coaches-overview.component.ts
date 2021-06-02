597a4f53c49a6a0d5687e1acbd977e3ebc6e4600
miguel <miguel.maieur@hotmail.com>
2021-06-02 13:45:01 +0200

    story11 | doing : add search on coachOverviewPage

--- src/app/user/coaches-overview/coaches-overview.component.ts
+++ src/app/user/coaches-overview/coaches-overview.component.ts
@@ -1,21 +1,29 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {Person} from '../../shared/model/person';
 import {PersonService} from '../../shared/service/person.service';
 import {CookieService} from 'ngx-cookie-service';
+import {CoachingTopic} from '../../shared/model/coaching-topic';
+import {InitService} from '../../shared/materialize/init.service';
 
 @Component({
   selector: 'app-coaches-overview',
   templateUrl: './coaches-overview.component.html',
   styleUrls: ['./coaches-overview.component.css']
 })
-export class CoachesOverviewComponent implements OnInit {
+export class CoachesOverviewComponent implements OnInit, AfterViewInit {
   private _coaches: Array<Person> = [];
+  private _coachesDisplay: Array<Person> = [];
+  private _topics: Array<string> = [];
   isLoading = true;
   hasCookie = false;
   cookieValue = '';
+  searchText = '';
+  private  minLengtTosearch = 3;
+  private _tempitem: Array<string> = [];
 
   constructor(private service: PersonService,
-              private cookieService: CookieService) {
+              private cookieService: CookieService,
+              private initService: InitService) {
   }
 
   ngOnInit(): void {
@@ -27,10 +35,17 @@ export class CoachesOverviewComponent implements OnInit {
     }
   }
 
+  ngAfterViewInit(): void {
+    this.initService.initFormSelect();
+  }
+
   private getCoaches(): void {
     this.service.getAllCoaches().subscribe(x => {
         this._coaches = x;
+        this._coachesDisplay = x;
         this.isLoading = false;
+        this.makeTopicSearchList();
+        this.initService.initFormSelect();
       },
       error => {
         console.log(error);
@@ -38,7 +53,65 @@ export class CoachesOverviewComponent implements OnInit {
       });
   }
 
+  private makeTopicSearchList(): void {
+    const temp: Array<CoachingTopic> = [];
+    this._coaches.forEach(x => x.coachingTopics.forEach(y => temp.push(y)));
+    this._topics = Array.from(new Set(temp.map(x => x.topic.toLowerCase())));
+  }
+
   get coaches(): Array<Person> {
-    return this._coaches;
+    return this._coachesDisplay;
+  }
+
+  get coachTopics(): Array<string> {
+    return this._topics;
+  }
+
+  onItemSelect(item: string[]): void {
+    this._coachesDisplay = [];
+    this._tempitem = [];
+    this._tempitem = item;
+
+    this.filterCoaches();
+  }
+
+  onTextChange(): void {
+    this._coachesDisplay = [];
+    this.filterCoaches();
+  }
+
+  private filterCoaches(): void{
+    if (this._tempitem.length === 0 && this.searchText.length <= 2){
+      this._coachesDisplay = this._coaches;
+    }
+    else if (this._tempitem.length === 0 && this.searchText.length > 2){
+      this._coaches.forEach(coach => {
+          if (this.searchText.length >= this.minLengtTosearch){
+            // tslint:disable-next-line:max-line-length
+            if (coach.firstName.toLowerCase().includes(this.searchText.toLowerCase()) || coach.lastName.toLowerCase().includes(this.searchText.toLowerCase()) || coach.email.toLowerCase().includes(this.searchText.toLowerCase())){
+              this._coachesDisplay.push(coach);
+            }
+          }
+          else{
+            this._coachesDisplay.push(coach);
+          }
+      });
+    }
+    else if (this._tempitem.length >= 1){
+      this._coaches.forEach(coach => {
+        const filteredTopic = coach.coachingTopics.filter(x => this._tempitem.some(y => x.topic.toLowerCase() === y));
+        if (filteredTopic.length > 0) {
+          if (this.searchText.length >= this.minLengtTosearch){
+            // tslint:disable-next-line:max-line-length
+            if (coach.firstName.toLowerCase().includes(this.searchText.toLowerCase()) || coach.lastName.toLowerCase().includes(this.searchText.toLowerCase()) || coach.email.toLowerCase().includes(this.searchText.toLowerCase())){
+              this._coachesDisplay.push(coach);
+            }
+          }
+          else{
+            this._coachesDisplay.push(coach);
+          }
+        }
+      });
+    }
   }
 }
